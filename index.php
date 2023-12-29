9f905deecb08997312df951d2110af91372a2883
Heru Aryasuta <heru.210180105@mhs.unimal.ac.id>
2023-12-29 15:06:44 +0700

    adjust some stuff

--- index.php
+++ index.php
@@ -1,67 +1,174 @@
 <?php
 include "koneksi.php";
 include "edit.php";
-include "hapus.php"
+include "hapus.php";
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tugas CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-      <div class="jumbotron mt-5">
-        <div class="card p-3">
-          <h2>Data Mahasiswa Kelompok 3</h2>
-        </div>
-        <div class="card">
-          <div class="card-body">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
-              Tambah Data
-            </button>
-          </div>
-          <table class="table table-bordered " cellspacing="0">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Tugas CRUD</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-N2gzGJm2W9qS7Qf5yC3t2aa1TqTxlkU02xtpzjJ2hR9Ozx+7sTwPWVQOdUJo+6b7HYi+g0vpJpxOt86PwRcR1g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <style>
+    body {
+      background-color: #f8f9fa;
+    }
+
+    .container {
+      margin-top: 50px;
+    }
+
+    .jumbotron {
+      background: linear-gradient(to right, #007bff, #0056b3);
+      color: white;
+      padding: 20px;
+      border-radius: 15px;
+      text-align: center;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+    }
+
+    .card {
+      margin-top: 20px;
+      border-radius: 15px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      transition: box-shadow 0.3s ease-in-out;
+    }
+
+    .card:hover {
+      box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    }
+
+    .card-header {
+      background-color: #007bff;
+      color: white;
+      border-bottom: none;
+      border-top-left-radius: 15px;
+      border-top-right-radius: 15px;
+      padding: 10px 20px;
+    }
+
+    .card-title {
+      margin-bottom: 0;
+      font-size: 1.5rem;
+    }
+
+    .card-body {
+      padding: 20px;
+      background-color: #f8f9fa;
+    }
+
+    .table th,
+    .table td {
+      text-align: center;
+      font-size: 1rem;
+    }
+
+    .btn-action {
+      margin-right: 5px;
+    }
+
+    .btn-warning,
+    .btn-danger {
+      transition: background-color 0.3s ease-in-out;
+    }
+
+    .btn-warning:hover,
+    .btn-danger:hover {
+      background-color: #dc3545;
+    }
+
+    .modal-content {
+      border-radius: 15px;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .modal-header {
+      background-color: #007bff;
+      color: white;
+      border-top-left-radius: 15px;
+      border-top-right-radius: 15px;
+      padding: 10px 20px;
+    }
+
+    .modal-title {
+      font-size: 1.5rem;
+    }
+
+    .modal-body {
+      padding: 20px;
+    }
+
+    .btn-primary,
+    .btn-danger {
+      width: 100%;
+      margin-top: 10px;
+    }
+
+    .current-date {
+      color: #6c757d;
+      margin-top: 10px;
+      font-size: 1.2rem;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container">
+    <div class="jumbotron">
+      <h2>Data Mahasiswa Kelompok 3</h2>
+      <p class="current-date" id="currentDate"></p>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <h5 class="card-title mb-0">Daftar Mahasiswa</h5>
+      </div>
+      <div class="card-body">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="fas fa-user-plus"></i> Tambah Data
+        </button>
+      </div>
+      <div class="card-body">
+        <table class="table table-bordered">
           <thead>
-				<tr>
-          <th>No</th>
-					<th>Nim</th>
-					<th>Nama</th>
-					<th>Kelas</th>
-					<th>Asal</th>
-          <th>Aksi</th>
-
-				</tr>
-			</thead>
-      <tbody>
-      <?php
-			$qsiswa = mysqli_query($db_conn,"SELECT * FROM biodata");
-      $i = 1;
-				while($data = mysqli_fetch_array($qsiswa)){
-                    $nim = $data['nim'];
-                    $nama = $data['nama'];
-                    $kelas = $data['kelas'];
-                    $asal =  $data['asal'];
-			?>
-        <tr>
-                <td><?=$i++;?></td>
-                <td><?=$nim;?></td>
-                <td><?=$nama;?></td>
-                <td><?=$kelas;?></td>
-                <td><?=$asal;?></td>
+            <tr>
+              <th>No</th>
+              <th>Nim</th>
+              <th>Nama</th>
+              <th>Kelas</th>
+              <th>Asal</th>
+              <th>Aksi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $qsiswa = mysqli_query($db_conn, "SELECT * FROM biodata");
+            $i = 1;
+            while ($data = mysqli_fetch_array($qsiswa)) {
+              $nim = $data['nim'];
+              $nama = $data['nama'];
+              $kelas = $data['kelas'];
+              $asal = $data['asal'];
+            ?>
+              <tr>
+                <td><?= $i++; ?></td>
+                <td><?= $nim; ?></td>
+                <td><?= $nama; ?></td>
+                <td><?= $kelas; ?></td>
+                <td><?= $asal; ?></td>
                 <td>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit<?=$nim;?>">
-                Edit
-                </button>
-                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#hapus<?=$nim;?>">
-                Hapus
-                </button>
+                  <button type="button" class="btn btn-primary btn-action" data-bs-toggle="modal" data-bs-target="#edit<?= $nim ?>">
+                    <i class="fas fa-edit"></i> Edit
+                  </button>
+                  <button type="button" class="btn btn-danger btn-action" data-bs-toggle="modal" data-bs-target="#hapus<?= $nim ?>">
+                    <i class="fas fa-trash-alt"></i> Hapus
+                  </button>
                 </td>
-        </tr>
-
+              </tr>
         <!--Edit Modal -->
       <div class="modal fade" id="edit<?=$nim;?>">
         <div class="modal-dialog">
@@ -125,6 +232,14 @@ include "hapus.php"
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script>
+    document.getElementById('currentDate').innerText = new Date().toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+  </script>
   </body>
       <!-- Modal -->
       <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
